5c9b50a1203e698b1feb0da6c36a6a8d6904033d
Yash Agrawal <yagrawl2@gmail.com>
2022-09-01 16:06:40 -0400

    fix .gitignore

--- client/src/container/checkout.tsx
+++ client/src/container/checkout.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppSelector } from '../app/hooks';
 import { selectOrderId } from '../reducers/flowSlice';
 import useScript from '../utils/hooks/useScript';
+import styles from './checkout.module.scss';
 
 const CDN_URL = "https://connect-sandbox.bolt.com";
 const PUBLISHABLE_KEY = "XRHslcMVq0Rf.D7GlPTeIRKUm.79646402d4ea75b6b89c35464905e9282be26340f9fa3cfaa157df3f356e67a1";
@@ -9,6 +10,7 @@ const BUTTON_BASE_URL = "https://connect-sandbox.bolt.com/v1/checkout_button";
 
 const Checkout = () => {
     const orderId = useAppSelector(selectOrderId);
+    const [checkoutSuccessful, setCheckoutSuccessful] = useState(false);
 
     useScript(`${CDN_URL}/track.js`, "bolt-track", "text/javascript", [{ key: "data-publishable-key", value: PUBLISHABLE_KEY }]);
     useScript(`${CDN_URL}/connect.js`, "bolt-connect", "text/javascript", [{ key: "data-publishable-key", value: PUBLISHABLE_KEY }]);
@@ -24,8 +26,9 @@ const Checkout = () => {
         onShippingDetailsComplete: function (address: any) {},
         onShippingOptionsComplete: function () {},
         onPaymentSubmit: function () {},
-        success: function (transaction: any, callback: any) {      
-          console.log("Checkout successful");
+        success: function (transaction: any, callback: any) {
+            setCheckoutSuccessful(true);
+            console.log("Checkout successful");
         },
         close: function () {},
     };
@@ -34,9 +37,15 @@ const Checkout = () => {
 
     return (
         <div>
-            <div data-tid="instant-bolt-checkout-button">
-                <object data={`${BUTTON_BASE_URL}?publishable_key=${PUBLISHABLE_KEY}`} aria-label={"bolt-checkout-button"}/>
-            </div>
+            <p className={styles.token}>Token <strong>{orderId}</strong></p>
+            {checkoutSuccessful ? (
+                <p className={styles.checkoutSuccessful}>Checkout successful!</p>
+            ) : (
+                    <div data-tid="instant-bolt-checkout-button">
+                        <object data={`${BUTTON_BASE_URL}?publishable_key=${PUBLISHABLE_KEY}`} aria-label={"bolt-checkout-button"}/>
+                    </div>
+                )
+            }
         </div>
     );
 }
